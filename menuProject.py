b58079b9fd9c543545c988594a45945a41af7dae
LopezDP <davidplopez@live.com>
2018-02-19 22:11:19 -0500

    Completing Google+ API Oauth SignIn & Authentication. Buggy.

--- menuProject.py
+++ menuProject.py
@@ -11,6 +11,16 @@ from flask import session as login_session
 import random
 import string
 
+# OAUTH IMPORTS
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+import requests
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json','r').read())['web']['client_id']
+
 app = Flask(__name__)
 
 # Create Session and connect to SQLite db
@@ -35,7 +45,86 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
         for x in range(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    #return "The current session state is %s" % login_session['state']
+    # RENDER the LOGIN.HTML TEMPLATE
+    return render_template('login.html', STATE=state)
+
+# Route handler to accept client-side calls from signInCallBack()
+@app.route('/gconnect', methods = ['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+    try:
+        #Upgrade the auth code into a credentials object
+        oath_flow = flow_from_clientsecrets('client_secrets.json', scope = '')
+        oath_flow.redirect_uri = 'postmessage'
+        credentials = oath_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check to be sure the access token is valid
+    access_token = credentials.access_token
+    url = ("https://www.googleapis.com/oath2/v1/tokeninfo?access_token=%s" % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    # Abort if there is an error with the access token
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+    # Ensure that the access_token is used by the intended person
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps('Token client ID does not match user ID'), 401)
+        print("Token client ID does not match app.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check to see if user is already logged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current User is already Signed In'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Save the access_token for later use dude
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+
+    return output
+
+
+
+
+
 
 # Building API Endpoints/Route Handlers (GET Request)
 @app.route('/')
